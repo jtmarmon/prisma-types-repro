c9c2483dc693647cd08851385e012648bfc4455b
Jason Marmon <jtmarmon@gmail.com>
2022-09-16 11:26:47 -0400

    repro

--- main.ts
+++ main.ts
@@ -3,23 +3,8 @@ import { PrismaClient } from "@prisma/client";
 async function main() {
   const prisma = new PrismaClient();
 
-  const userRoles = await prisma.user
-    .findUnique({
-      where: { UserID: 1 },
-    })
-    .UserRole({
-      select: {
-        Role: {
-          select: {
-            Id: true,
-            Name: true,
-          },
-        },
-      },
-    });
-
-  console.log(userRoles);
-  prisma.$disconnect();
+  const user = await prisma.user.findFirst();
+  console.log(user.name);
 }
 
 main();
